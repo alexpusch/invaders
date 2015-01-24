b555817f29b6359de071e1b5a53d47d5d7e7144a
Alex Puschinsky <alexpoo@gmail.com>
2015-01-25 01:01:06 +0200

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,67 @@ notifier = require("node-notifier")
 // generated on 2015-01-17 using generator-gulp-webapp 0.2.0
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+fs = require("fs")
+_ = require("lodash")
+
+notify = function(err){
+  notifier.notify({
+    'title': 'Coffeescript error',
+    'message': err.toString(),
+    'sound': true
+  });
+  $.util.log(err); 
+  this.emit("end") 
+}
+
+gulp.task('coffeescript', function () {
+  return gulp.src(['app/**/*.coffee', 'test/**/*.coffee'])
+    .pipe($.coffee().on('error', notify))
+    .pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('browserify', ['coffeescript'], function() {
+  return browserify('./.tmp/scripts/main', {debug: true})
+      .bundle().on('error', notify)
+      //Pass desired output filename to vinyl-source-stream
+      .pipe(source('bundle.js'))
+      // Start piping stream to tasks!
+      .pipe(gulp.dest('./.tmp/scripts'));
+});
+
+gulp.task('spec-browserify', ['coffeescript'], function() {
+  base = __dirname + '/.tmp/spec'
+  files = fs.readdirSync(base)
+  streams = _(files).filter(function(filename){
+    return filename.match(/.*_specs\.js/)
+  }).map(function(filename){
+    console.log(base + "/" + filename)
+    return fs.createReadStream(base + "/" + filename);
+  })
+  .value()
+
+  return browserify(streams, {debug: true})
+      .bundle().on('error', notify)
+      //Pass desired output filename to vinyl-source-stream
+      .pipe(source('spec-bundle.js'))
+      // Start piping stream to tasks!
+      .pipe(gulp.dest('./.tmp/spec'));
+});
+
+gulp.task('test', ['scripts'], function(){
+  return gulp.src(".tmp/spec/**/*.js")
+    .pipe($.jasmine())
+});
+
+gulp.task('scripts', ['coffeescript', 'browserify', 'spec-browserify'])
+gulp.task('spec-scripts', ['coffeescript', 'spec-browserify'])
 
 gulp.task('styles', function () {
   return gulp.src('app/styles/main.css')
     .pipe($.autoprefixer({browsers: ['last 1 version']}))
     .pipe(gulp.dest('.tmp/styles'));
-});
-
-gulp.task('scripts', function () {
-  return gulp.src('app/scripts/**/*.coffee')
-    .pipe($.coffee().on('error', function(err){ 
-      notifier.notify({
-        'title': 'Coffeescript error',
-        'message': err.toString(),
-        'sound': true
-      });
-      $.util.log(err); 
-      this.emit("end") } ))
-    .pipe(gulp.dest('.tmp/scripts'));
 });
 
 gulp.task('jshint', function () {
@@ -78,10 +121,12 @@ gulp.task('connect', ['styles', 'scripts', 'fonts'], function () {
     .use(require('connect-livereload')({port: 35729}))
     .use(serveStatic('.tmp'))
     .use(serveStatic('app'))
+    .use('/test', serveStatic('test'))
     // paths to bower_components should be relative to the current file
     // e.g. in app/index.html you should use ../bower_components
     .use('/bower_components', serveStatic('bower_components'))
-    .use(serveIndex('app'));
+    .use(serveIndex('app'))
+    .use('/test', serveIndex('test'));
 
   require('http').createServer(app)
     .listen(9000)
@@ -117,6 +162,7 @@ gulp.task('watch', ['connect'], function () {
 
   gulp.watch('app/styles/**/*.css', ['styles']);
   gulp.watch('app/scripts/**/*.coffee', ['scripts']);
+  gulp.watch('test/spec/**/*.coffee', ['spec-scripts']);
   gulp.watch('bower.json', ['wiredep']);
 });
 
@@ -127,3 +173,4 @@ gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () 
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
+
